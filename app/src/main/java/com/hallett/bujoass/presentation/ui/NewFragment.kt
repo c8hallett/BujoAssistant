a39afce4bc1b2726fe405e1553b88637786ba583
Caitlyn Hallett <65869260+c8hallett@users.noreply.github.com>
2021-06-05 21:10:00 -0400

    Moved NewFragment's state logic into the NewFragmentViewModel

--- app/src/main/java/com/hallett/bujoass/presentation/ui/NewFragment.kt
+++ app/src/main/java/com/hallett/bujoass/presentation/ui/NewFragment.kt
@@ -7,17 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.hallett.bujoass.R
 import com.hallett.bujoass.databinding.FragmentNewBinding
 import com.hallett.bujoass.domain.Scope
-import java.text.SimpleDateFormat
-import java.util.Calendar
+import com.hallett.bujoass.presentation.viewmodel.NewFragmentViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class NewFragment: Fragment() {
-    private lateinit var binding: FragmentNewBinding
-    private var currentlySelectedDateTime: Calendar = Calendar.getInstance()
-    private var scope: Scope = Scope.DAY
-
+    private lateinit var binding: FragmentNewBinding;
+    private val viewModel: NewFragmentViewModel by lazy {
+        ViewModelProvider(this).get(NewFragmentViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,7 +34,9 @@ class NewFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.run{
-            dateValue.text = SimpleDateFormat("MMM DD, YYYY").format(currentlySelectedDateTime.time)
+            lifecycleScope.launch {
+                viewModel.observeSelectedDate().collect{ dateValue.text = it }
+            }
             pickScopeSpn.apply {
                 adapter = ArrayAdapter.createFromResource(context,
                     R.array.scope_array, android.R.layout.simple_spinner_item)
@@ -39,12 +44,7 @@ class NewFragment: Fragment() {
             pickDateBtn.setOnClickListener {
                 DatePickerDialog(requireContext()).apply {
                     setOnDateSetListener { _, year, month, dayOfMonth ->
-                        currentlySelectedDateTime.apply{
-                            set(Calendar.YEAR, year)
-                            set(Calendar.MONTH, month)
-                            set(Calendar.DATE, dayOfMonth)
-                            dateValue.text = SimpleDateFormat("MMM DD, YYYY").format(currentlySelectedDateTime.time)
-                        }
+                        viewModel.updateSelectedTime(year, month, dayOfMonth)
                     }
                     datePicker.minDate = System.currentTimeMillis()
                 }.show()
--- app/src/main/java/com/hallett/bujoass/presentation/viewmodel/NewFragmentViewModel.kt
+++ app/src/main/java/com/hallett/bujoass/presentation/viewmodel/NewFragmentViewModel.kt
@@ -1,7 +1,30 @@
 package com.hallett.bujoass.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NewFragmentViewModel: ViewModel() {
-    
+    private var currentlySelectedDateTime: Calendar = Calendar.getInstance()
+    private val selectedDateFlow = MutableStateFlow(formatUiDateString())
+
+    fun updateSelectedTime(year: Int, month: Int, date: Int) {
+        viewModelScope.launch {
+            currentlySelectedDateTime = currentlySelectedDateTime.apply {
+                set(Calendar.YEAR, year)
+                set(Calendar.MONTH, month)
+                set(Calendar.DATE, date)
+            }
+            selectedDateFlow.emit(formatUiDateString())
+        }
+    }
+
+    fun observeSelectedDate(): Flow<String> = selectedDateFlow
+
+    private fun formatUiDateString(): String = SimpleDateFormat("MMM DD, YYYY").format(currentlySelectedDateTime.time)
+
 }
