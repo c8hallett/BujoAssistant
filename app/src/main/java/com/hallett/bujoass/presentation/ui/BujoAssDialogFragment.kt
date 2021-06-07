6f2c080c846c2d572e4beb6fa9d83fe3e2813d8c
Caitlyn Hallett <65869260+c8hallett@users.noreply.github.com>
2021-06-06 21:48:29 -0400

    Custom Base DialogFragment

---
+++ app/src/main/java/com/hallett/bujoass/presentation/ui/BujoAssDialogFragment.kt
@@ -0,0 +1,28 @@
+package com.hallett.bujoass.presentation.ui
+
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProvider
+import com.hallett.bujoass.R
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
+
+class BujoAssDialogFragment: DialogFragment(), KodeinAware {
+
+    override val kodein: Kodein by closestKodein()
+    protected val vmpfactory: ViewModelProvider.Factory by instance()
+
+    open val isFullScreen: Boolean = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (isFullScreen) {
+            setStyle(STYLE_NORMAL, R.style.AppTheme_Dialog_FullScreen)
+        } else {
+            setStyle(STYLE_NORMAL, R.style.AppTheme_Dialog)
+        }
+    }
+}
