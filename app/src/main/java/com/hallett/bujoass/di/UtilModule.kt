adb0daacac552d6f7e669ba9808307f74d4ba701
Caitlyn Hallett <65869260+c8hallett@users.noreply.github.com>
2021-06-27 00:59:48 -0400

    Added a factory binding for IScopeOperator

--- app/src/main/java/com/hallett/bujoass/di/UtilModule.kt
+++ app/src/main/java/com/hallett/bujoass/di/UtilModule.kt
@@ -4,13 +4,24 @@ import androidx.lifecycle.ViewModelProvider
 import com.hallett.bujoass.domain.model.DScope
 import com.hallett.bujoass.domain.model.DScopeInstance
 import com.hallett.bujoass.domain.usecase.mapper.*
+import com.hallett.bujoass.domain.usecase.scope_operations.*
 import com.hallett.bujoass.presentation.model.PScope
 import com.hallett.bujoass.presentation.model.PScopeInstance
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
+import org.kodein.di.generic.factory
 import org.kodein.di.generic.instance
 import org.kodein.di.generic.singleton
+import java.util.*
 
 val utilModule = Kodein.Module("util_module") {
     bind<ViewModelProvider.Factory>() with singleton { KodeinViewModelProviderFactory(kodein) }
+    bind<IScopeOperator>() with factory { scope: DScope, rawDate: Date ->
+        when(scope){
+            DScope.DAY -> DayOperator(rawDate)
+            DScope.WEEK -> WeekOperator(rawDate)
+            DScope.MONTH -> MonthOperator(rawDate)
+            DScope.YEAR -> YearOperator(rawDate)
+        }
+    }
 }
