eff28967bcfc979f84c25513b7d4d841261f14b8
Caitlyn Hallett <65869260+c8hallett@users.noreply.github.com>
2021-06-27 14:09:21 -0400

    Updated ViewTask Fragment to be more dynamic

--- app/src/main/java/com/hallett/bujoass/presentation/ui/view_task/ViewTaskDialogFragment.kt
+++ app/src/main/java/com/hallett/bujoass/presentation/ui/view_task/ViewTaskDialogFragment.kt
@@ -4,17 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.chip.ChipGroup
 import com.hallett.bujoass.databinding.FragmentViewTaskBinding
 import com.hallett.bujoass.domain.model.TaskStatus
 import com.hallett.bujoass.presentation.gone
 import com.hallett.bujoass.presentation.model.PScope
-import com.hallett.bujoass.presentation.setVis
+import com.hallett.bujoass.presentation.model.PScopeInstance
 import com.hallett.bujoass.presentation.ui.BujoAssDialogFragment
 import com.hallett.bujoass.presentation.visible
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import java.util.*
 
 class ViewTaskDialogFragment: BujoAssDialogFragment() {
     override val isFullScreen: Boolean = false
@@ -50,52 +53,152 @@ class ViewTaskDialogFragment: BujoAssDialogFragment() {
     }
 
     private fun hookupViewModelObservers(){
-        binding.apply {
-            lifecycleScope.launch {
-                val taskId = arguments?.getLong(EXTRA_TASK_ID) ?: throw IllegalStateException("Launched ViewTaskDialogFragment without passing in task id. Use ViewTaskDialogFragment.newInstance() instead.")
-                viewModel.observeTask(taskId).collect {
+        val taskId = arguments?.getLong(EXTRA_TASK_ID)
+            ?: throw IllegalStateException(
+                "Launched ViewTaskDialogFragment without passing in task id. " +
+                "Use ViewTaskDialogFragment.newInstance() instead."
+            )
+        lifecycleScope.launch {
+            viewModel.observeTask(taskId).collect {
+                binding.run {
                     taskName.text = it.taskName
                     scopeLabel.text = it.scopeLabel
                     when(it.status){
                         TaskStatus.INCOMPLETE -> {
-                            rescheduleBtn.visible()
-                            deferBtn.visible()
-                            completeBtn.visible()
-                            cancelBtn.visible()
-                            rescheduleBtn.text = "reschedule"
-                            when(it.scope){
-                                PScope.NONE -> {
-                                    deferBtn.gone()
-                                    rescheduleBtn.text = "schedule"
+                            scheduleBlock.visible()
+                            statusBlock.visible()
+                            val options = when(it.scope){
+                                PScope.NONE -> arrayOf(
+                                    Button(context).apply {
+                                        text = "schedule"
+                                        setOnClickListener {
+                                            // TODO: schedule task
+                                        }
+                                    }
+                                )
+                                PScope.DAY -> when {
+                                    it.isCurrent -> arrayOf(
+                                        Button(context).apply {
+                                            text = "do tomorrow"
+                                            setOnClickListener {
+                                                viewModel.deferTask()
+                                            }
+                                        },
+                                        Button(context).apply {
+                                            text = "reschedule"
+                                            setOnClickListener {
+                                                // TODO: schedule task
+                                            }
+                                        }
+                                    )
+                                    else -> arrayOf(
+                                        Button(context).apply {
+                                            text = "do today"
+                                            setOnClickListener {
+                                                viewModel.rescheduleTask(PScopeInstance(PScope.DAY, Date()))
+                                            }
+                                        },
+                                        Button(context).apply {
+                                            text = "reschedule"
+                                            setOnClickListener {
+                                                // TODO: schedule task
+                                            }
+                                        }
+                                    )
                                 }
-                                PScope.DAY -> {
-                                    // TODO: only say tomorrow when date of task is "today"
-                                    deferBtn.text = "tomorrow"
-                                }
-                                // TODO: for scopes bigger than "DAY"
-                                PScope.WEEK -> {
-                                    deferBtn.text = "next week"
-                                }
-                                PScope.MONTH -> {
-                                    deferBtn.text = "next month"
-                                }
-                                PScope.YEAR -> {
-                                    deferBtn.text = "next year"
-                                }
+                                PScope.WEEK -> arrayOf(
+                                    Button(context).apply {
+                                        text = "do today"
+                                        setOnClickListener {
+                                            viewModel.rescheduleTask(PScopeInstance(PScope.DAY, Date()))
+                                        }
+                                    },
+                                    Button(context).apply {
+                                        text = "do next week"
+                                        setOnClickListener {
+                                            viewModel.deferTask()
+                                        }
+                                    },
+                                    Button(context).apply {
+                                        text = "reschedule"
+                                        setOnClickListener {
+                                            // TODO: schedule task
+                                        }
+                                    }
+                                )
+                                PScope.MONTH -> arrayOf(
+                                    Button(context).apply {
+                                        text = "do today"
+                                        setOnClickListener {
+                                            viewModel.rescheduleTask(PScopeInstance(PScope.DAY, Date()))
+                                        }
+                                    },
+                                    Button(context).apply {
+                                        text = "do next month"
+                                        setOnClickListener {
+                                            viewModel.deferTask()
+                                        }
+                                    },
+                                    Button(context).apply {
+                                        text = "reschedule"
+                                        setOnClickListener {
+                                            // TODO: schedule task
+                                        }
+                                    }
+                                )
+                                PScope.YEAR -> arrayOf(
+                                    Button(context).apply {
+                                        text = "do today"
+                                        setOnClickListener {
+                                            viewModel.rescheduleTask(PScopeInstance(PScope.DAY, Date()))
+                                        }
+                                    },
+                                    Button(context).apply {
+                                        text = "do next year"
+                                        setOnClickListener {
+                                            viewModel.deferTask()
+                                        }
+                                    },
+                                    Button(context).apply {
+                                        text = "reschedule"
+                                        setOnClickListener {
+                                            // TODO: schedule task
+                                        }
+                                    }
+                                )
                             }
+                            val statusOptions = arrayOf(
+                                Button(context).apply {
+                                    text = "complete"
+                                    setOnClickListener {
+                                        viewModel.updateStatus(TaskStatus.COMPLETE)
+                                    }
+                                },
+                                Button(context).apply {
+                                    text = "cancel"
+                                    setOnClickListener {
+                                        viewModel.updateStatus(TaskStatus.CANCELLED)
+                                    }
+                                }
+                            )
+                            scheduleBlock.replaceViews(*options)
+                            statusBlock.replaceViews(*statusOptions)
                         }
                         TaskStatus.CANCELLED -> {
-                            rescheduleBtn.gone()
-                            deferBtn.gone()
-                            completeBtn.gone()
-                            cancelBtn.visible()
-                            cancelBtn.text = "uncancel task"
+                            scheduleBlock.gone()
+                            statusBlock.visible()
+                            val statusButton = Button(context).apply {
+                                text = "uncancel"
+                                setOnClickListener {
+                                    viewModel.updateStatus(TaskStatus.INCOMPLETE)
+                                }
+                            }
+                            statusBlock.replaceViews(statusButton)
                         }
                         TaskStatus.COMPLETE -> {
-                            rescheduleBtn.gone()
-                            deferBtn.gone()
-                            completeBtn.gone()
-                            cancelBtn.gone()
+                            scheduleBlock.gone()
+                            statusBlock.gone()
+                            // Nothing can be modified about the task--only for viewing (or deleting)
                         }
                     }
                 }
@@ -103,13 +206,14 @@ class ViewTaskDialogFragment: BujoAssDialogFragment() {
         }
     }
 
-    private fun setOnClickListeners() {
-        binding.run {
-            deferBtn.setOnClickListener {  }
-            rescheduleBtn.setOnClickListener {  }
-            deleteBtn.setOnClickListener {  }
-            cancelBtn.setOnClickListener {  }
-            completeBtn.setOnClickListener {  }
+    private fun ChipGroup.replaceViews(vararg views: View) {
+        removeAllViews()
+        views.forEach {
+            addView(it)
         }
+    }
+
+    private fun setOnClickListeners() {
+        binding.deleteBtn.setOnClickListener { viewModel.deleteTask() }
     }
 }
--- app/src/main/java/com/hallett/bujoass/presentation/ui/view_task/ViewTaskDialogFragmentViewModel.kt
+++ app/src/main/java/com/hallett/bujoass/presentation/ui/view_task/ViewTaskDialogFragmentViewModel.kt
@@ -1,14 +1,26 @@
 package com.hallett.bujoass.presentation.ui.view_task
 
+import androidx.lifecycle.viewModelScope
+import com.hallett.bujoass.domain.model.TaskStatus
+import com.hallett.bujoass.domain.usecase.modify_task.IDeferTaskUseCase
+import com.hallett.bujoass.domain.usecase.modify_task.IDeleteTaskUseCase
+import com.hallett.bujoass.domain.usecase.modify_task.IModifyTaskStatusUseCase
+import com.hallett.bujoass.domain.usecase.modify_task.IRescheduleTaskUseCase
 import com.hallett.bujoass.domain.usecase.observe_task.IObserveSingleTaskUseCase
 import com.hallett.bujoass.presentation.ui.BujoAssViewModel
 import com.hallett.bujoass.presentation.format.Formatter
 import com.hallett.bujoass.presentation.model.PScopeInstance
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import java.util.*
 
 class ViewTaskDialogFragmentViewModel(
     private val observeSingleTaskUseCase: IObserveSingleTaskUseCase,
+    private val modifyTaskStatusUseCase: IModifyTaskStatusUseCase,
+    private val rescheduleTaskUseCase: IRescheduleTaskUseCase,
+    private val deferTaskUseCase: IDeferTaskUseCase,
+    private val deleteTaskUseCase: IDeleteTaskUseCase,
     private val scopeFormatter: Formatter<PScopeInstance>,
 ): BujoAssViewModel() {
 
@@ -23,6 +35,30 @@ class ViewTaskDialogFragmentViewModel(
                 it.status,
                 it.isCurrentScope,
             )
+        }
+    }
+
+    fun updateStatus(status: TaskStatus) {
+        viewModelScope.launch {
+            modifyTaskStatusUseCase.execute(taskId, status)
+        }
+    }
+
+    fun deferTask() {
+        viewModelScope.launch {
+            deferTaskUseCase.execute(taskId)
+        }
+    }
+
+    fun rescheduleTask(scope: PScopeInstance) {
+        viewModelScope.launch {
+            rescheduleTaskUseCase.execute(taskId, scope)
+        }
+    }
+
+    fun deleteTask() {
+        viewModelScope.launch {
+            deferTaskUseCase.execute(taskId)
         }
     }
 }
