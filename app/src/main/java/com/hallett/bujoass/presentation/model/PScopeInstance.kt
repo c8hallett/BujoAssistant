c1ffd50d27966da79d9050aaa8aff595b6ccd946
Caitlyn Hallett <65869260+c8hallett@users.noreply.github.com>
2021-07-05 13:19:23 -0400

    Made PScopeInstance parcelable

--- app/src/main/java/com/hallett/bujoass/presentation/model/PScopeInstance.kt
+++ app/src/main/java/com/hallett/bujoass/presentation/model/PScopeInstance.kt
@@ -1,8 +1,11 @@
 package com.hallett.bujoass.presentation.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.util.*
 
-data class PScopeInstance(val scope: PScope, val date: Date){
+@Parcelize
+data class PScopeInstance(val scope: PScope, val date: Date): Parcelable{
     companion object{
         val NONE = PScopeInstance(PScope.NONE, Date(0))
     }
